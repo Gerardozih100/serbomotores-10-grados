fd6ca3e1059464b22584be7f788817871fc248a0
Gerardozih100 <106816077+Gerardozih100@users.noreply.github.com>
2022-06-08 23:50:27 -0500

    Se agrego la parte del Bluetooth pal control del servo

--- main.ts
+++ main.ts
@@ -14,6 +14,25 @@ input.onButtonPressed(Button.B, function () {
     }
     servos.P0.setAngle(Posicion)
 })
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        basic.showArrow(ArrowNames.North)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        basic.showArrow(ArrowNames.South)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        basic.showArrow(ArrowNames.West)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        basic.showArrow(ArrowNames.East)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        basic.showString("A")
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        basic.showString("B")
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        basic.showString("C")
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        basic.showString("D")
+    }
+})
 let Posicion = 0
 Posicion = 90
 servos.P0.setAngle(Posicion)
